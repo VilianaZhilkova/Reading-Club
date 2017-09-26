738302e4b042b5c8c20a497cf6591a89f4ec9b60
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-09-26 11:59:12 +0300

    Created ReadingClub.Web Project and class libraries ReadingClub.Data, ReadingClub.Data.Common and ReadingClub.Data.Models

---
+++ ReadingClub/ReadingClub.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReadingClub.Web.MvcApplication" Language="C#" %>
---
+++ ReadingClub/ReadingClub.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReadingClub.Web.Startup))]
+namespace ReadingClub.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
