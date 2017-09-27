6c49c2e2811699a086b14afcc2f61bd74ffc63c7
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-09-27 08:28:34 +0300

    Extracted ApplicationDbContext and ApplicationUser(renamed to User) into separate projects

---
+++ ReadingClub/Data/ReadingClub.Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ReadingClub.Data.Models;
+using System.Data.Entity;
+
+namespace ReadingClub.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext()
+            : base("ReadingClubDb", throwIfV1Schema: false)
+        {
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
--- ReadingClub/ReadingClub.Web/App_Start/Startup.Auth.cs
+++ ReadingClub/ReadingClub.Web/App_Start/Startup.Auth.cs
@@ -6,6 +6,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using ReadingClub.Web.Models;
+using ReadingClub.Data.Models;
+using ReadingClub.Data;
 
 namespace ReadingClub.Web
 {
@@ -30,7 +32,7 @@ namespace ReadingClub.Web
                 {
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, User>(
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
