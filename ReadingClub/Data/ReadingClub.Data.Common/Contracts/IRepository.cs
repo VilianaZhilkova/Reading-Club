303603bbeb5cf9f48891b1ae2bfd533456922d2d
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-09-28 19:27:22 +0300

    Created IRepository.cs and EfRepository.cs

---
+++ ReadingClub/Data/ReadingClub.Data.Common/Contracts/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+using ReadingClub.Data.Models.Contracts;
+
+namespace ReadingClub.Data.Common.Contracts
+{
+    public interface IRepository<T> where T : class, IAuditable, IDeletable
+    {
+        IQueryable<T> GetAll { get; }
+
+        IQueryable<T> GetAllWithDeleted { get; }
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+
+        IQueryable<T> Search(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ ReadingClub/Data/ReadingClub.Data.Common/EfRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+using ReadingClub.Data.Common.Contracts;
+using ReadingClub.Data.Models.Contracts;
+
+namespace ReadingClub.Data.Common
+{
+    public class EfRepository<T> : IRepository<T>
+            where T : class, IAuditable, IDeletable
+    {
+        public EfRepository(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("An instance of DbContext is required to use this repository.", nameof(context));
+            }
+
+            this.Context = context;
+            this.DbSet = this.Context.Set<T>();
+        }
+
+        private IDbSet<T> DbSet { get; }
+
+        private DbContext Context { get; }
+
+        public IQueryable<T> GetAll
+        {
+            get
+            {
+                return this.DbSet.Where(x => !x.IsDeleted);
+            }
+        }
+
+        public IQueryable<T> GetAllWithDeleted
+        {
+            get
+            {
+                return this.DbSet;
+            }
+        }
+
+        public T GetById(object id)
+        {
+            var item = this.DbSet.Find(id);
+            if (item.IsDeleted)
+            {
+                return null;
+            }
+
+            return item;
+        }
+
+        public void Add(T entity)
+        {
+            this.DbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.UtcNow;
+        }
+
+        public void Update(T entity)
+        {
+            var entry = this.Context.Entry(entity);
+
+            entry.State = EntityState.Modified;
+        }
+
+        public IQueryable<T> Search(Expression<Func<T, bool>> predicate)
+        {
+            return this.DbSet.Where(predicate);
+        }
+    }
+}
