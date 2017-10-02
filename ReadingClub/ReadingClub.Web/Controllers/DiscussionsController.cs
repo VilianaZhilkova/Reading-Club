4037a8b73b40330d9b0519238eff43fac5bd899e
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-10-02 19:38:54 +0300

    Navigation bar changed. Created DiscussionsController.cs .

---
+++ ReadingClub/ReadingClub.Web/Controllers/DiscussionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReadingClub.Web.Controllers
+{
+    public class DiscussionsController : Controller
+    {
+        // GET: Discussions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
