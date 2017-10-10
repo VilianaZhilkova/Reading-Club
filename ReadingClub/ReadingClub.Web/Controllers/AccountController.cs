b9d84f8abda01a60cd5a02c2c4ebaab1b0a13c65
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-10-11 00:09:19 +0300

    Register method in AccountController changed.

--- ReadingClub/ReadingClub.Web/Controllers/AccountController.cs
+++ ReadingClub/ReadingClub.Web/Controllers/AccountController.cs
@@ -157,7 +157,8 @@ namespace ReadingClub.Web.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
+                    result = UserManager.AddToRole(user.Id, "User");
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
