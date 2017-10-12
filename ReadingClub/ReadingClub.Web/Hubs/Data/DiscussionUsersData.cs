4302bb19a5914ed488e73dd87a9a191b437b4c57
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-10-12 06:27:19 +0300

    Changes in DiscussionUsersData.cs.

--- ReadingClub/ReadingClub.Web/Hubs/Data/DiscussionUsersData.cs
+++ ReadingClub/ReadingClub.Web/Hubs/Data/DiscussionUsersData.cs
@@ -1,36 +1,38 @@
-﻿using System.Configuration;
+﻿using ReadingClub.Data.Common.Contracts;
+using ReadingClub.Data.Models;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace ReadingClub.Web.Hubs.Data
 {
     public class DiscussionUsersData : IDiscussionUsersData
     {
+        private readonly IRepository<Discussion> discussions;
+
+        public DiscussionUsersData(IRepository<Discussion> discussions)
+        {
+            this.discussions = discussions;
+        }
         public void GetData()
         {
+            var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ReadingClubDb"].ConnectionString);
+            connection.Open();
 
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ReadingClubDb"].ConnectionString))
+            var query = this.discussions.GetAll.Select(x => x.Users);
+            var commandText = query.ToString();
+            var command = new SqlCommand(commandText, connection);
+
+            SqlDependency dependency = new SqlDependency(command);
+            dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+
+            if (connection.State == ConnectionState.Closed)
             {
                 connection.Open();
-                using (SqlCommand command = new SqlCommand(@"SELECT d.Discussion_Id, d.User_Id ,u.UserName
-                    FROM dbo.DiscussionUsers d
-                    JOIN dbo.AspNetUsers u
-                    ON d.User_Id = u.Id", connection))
-                {
-                    // Make sure the command object does not already have
-                    // a notification object associated with it.
-                    command.Notification = null;
-
-                    SqlDependency dependency = new SqlDependency(command);
-                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
-
-                    if (connection.State == ConnectionState.Closed)
-                    {
-                        connection.Open();
-                    }
-                    var reader = command.ExecuteReader();
-                }
             }
+
+            var reader = command.ExecuteReader();
         }
 
         private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
